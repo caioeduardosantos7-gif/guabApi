c083045ed7cb6e0adc0580eec179438b32b740a9
unknown <caioeduardosantos7@gmail.com>
2026-03-22 02:06:31 -0300

    first commit

---
+++ src/main/java/com/dashapi/demo/ApiDashApplication.java
@@ -0,0 +1,13 @@
+package com.dashapi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDashApplication.class, args);
+	}
+
+}
